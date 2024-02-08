7fad37adad23da5d327501b95ddd1275c81ab854
Sofía Rocher <76635239+sofiarocher@users.noreply.github.com>
2024-02-08 17:26:29 -0300

    links

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
-import Footer from './components/Footer'
-import Nav from './components/Navbar'
-import Home from './pages/Home'
-import Contacto from './pages/Contacto'
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
+import Nav from './components/Navbar';
+import Home from './pages/Home';
+import Contacto from './pages/Contacto';
 import Institucional from './pages/Institucional';
 import Servicios from './pages/Servicios';
 
 function App() {
-
   return (
     <>
       <Router>
         <Nav />
-          <Routes>
-            <Route path='/' Component={Home} />
-            <Route path='/institucional' Component={Institucional}/>
-            <Route path='/servicios' Component={Servicios}/>
-            <Route path='/contacto' Component={Contacto}/>
-          </Routes>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/institucional' element={<Institucional />} />
+          <Route path='/servicios' element={<Servicios />} />
+          <Route path='/contacto' element={<Contacto />} />
+        </Routes>
         <Footer />
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
